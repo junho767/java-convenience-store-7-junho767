94ba4989c27b20c806e62d6a49c14d0e79cdcc42
junho767 <junho767@naver.com>
2024-11-10 20:09:32 +0900

    refactor: 메서드 추출하여 리펙터링

--- src/main/java/store/model/ProductLoader.java
+++ src/main/java/store/model/ProductLoader.java
@@ -14,27 +14,46 @@ public class ProductLoader {
             br.readLine();
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                String name = values[0];
-                int price = Integer.parseInt(values[1]);
-                int quantity = Integer.parseInt(values[2]);
-                Promotion promotion = Promotion.fromString(values[3]);
-                if (!productMap.containsKey(name) && promotion != Promotion.NULL) {
-                    Product product = new Product(name, price, 0, promotion.getName());
-                    product.setPromotionQuantity(quantity);
-                    productMap.put(name, product);
-                }
-                if (productMap.containsKey(name) && promotion == Promotion.NULL) {
-                    Product product = productMap.get(name);
-                    product.setQuantity(quantity);
-                }
-                if (!productMap.containsKey(name) && promotion == Promotion.NULL) {
-                    Product product = new Product(name, price, quantity, promotion.getName());
-                    productMap.put(name, product);
-                }
+                processLine(line, productMap);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void processLine(String line, Map<String, Product> productMap) {
+        String[] values = line.split(",");
+        String name = values[0];
+        int price = Integer.parseInt(values[1]);
+        int quantity = Integer.parseInt(values[2]);
+        Promotion promotion = Promotion.fromString(values[3]);
+
+        if (!productMap.containsKey(name) && promotion != Promotion.NULL) {
+            createNewProductWithPromotion(name, price, quantity, promotion, productMap);
+        }
+
+        if (productMap.containsKey(name) && promotion == Promotion.NULL) {
+            updateProductQuantity(name, quantity, productMap);
+        }
+
+        if (!productMap.containsKey(name) && promotion == Promotion.NULL) {
+            createNewProductWithoutPromotion(name, price, quantity, productMap);
+        }
+    }
+
+    private static void createNewProductWithPromotion(String name, int price, int quantity, Promotion promotion, Map<String, Product> productMap) {
+        Product product = new Product(name, price, 0, promotion.getName());
+        product.setPromotionQuantity(quantity);
+        productMap.put(name, product);
+    }
+
+    private static void createNewProductWithoutPromotion(String name, int price, int quantity, Map<String, Product> productMap) {
+        Product product = new Product(name, price, quantity, Promotion.NULL.getName());
+        productMap.put(name, product);
+    }
+
+    private static void updateProductQuantity(String name, int quantity, Map<String, Product> productMap) {
+        Product product = productMap.get(name);
+        product.setQuantity(quantity);
     }
 }
