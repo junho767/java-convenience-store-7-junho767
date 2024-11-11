2ba5b8e0886a7c13dce4307afaf6077540107128
junho767 <junho767@naver.com>
2024-11-11 18:32:14 +0900

    refactor: 영수증 출력 책임 분리 및 출력 메세지 추가

---
+++ src/main/java/store/model/Receipt.java
@@ -0,0 +1,67 @@
+package store.model;
+
+import store.message.ReceiptMessage;
+import store.view.OutputView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+public class Receipt {
+    private final Map<String, PurchaseItem> purchase;
+    private final Map<String, PurchaseItem> freeItems;
+    private final int membershipDiscountPrice;
+    private final int totalAmount;
+    private int totalPromotionDiscount;
+    private int totalPurchasePrice;
+
+    public Receipt(Map<String, PurchaseItem> purchase, Map<String, PurchaseItem> freeItems,
+                   int membershipDiscountPrice, int totalAmount) {
+        this.purchase = purchase;
+        this.freeItems = freeItems;
+        this.membershipDiscountPrice = membershipDiscountPrice;
+        this.totalAmount = totalAmount;
+        this.totalPromotionDiscount = 0;
+        this.totalPurchasePrice = 0;
+
+    }
+
+    public void print() {
+        NumberFormat currencyFormat = NumberFormat.getInstance(Locale.KOREA);
+
+        OutputView.printStore();
+
+        totalPurchasePrice = purchaseItems(currencyFormat, totalPurchasePrice);
+        totalPromotionDiscount = freeItems(totalPromotionDiscount);
+
+        OutputView.printLine();
+
+        printPriceDetails(currencyFormat);
+    }
+
+    private int purchaseItems(NumberFormat currencyFormat, int totalPurchasePrice) {
+        for (PurchaseItem purchaseItem : purchase.values()) {
+            totalPurchasePrice += purchaseItem.getPrice();
+            OutputView.printProductFormat(purchaseItem, currencyFormat);
+        }
+        return totalPurchasePrice;
+    }
+
+    private int freeItems(int totalPromotionDiscount) {
+        OutputView.printFreeItem();
+        for (PurchaseItem freeItem : freeItems.values()) {
+            totalPromotionDiscount += freeItem.getQuantity() * freeItem.getPrice();
+            if (freeItem.getQuantity() > 0) {
+                OutputView.printFreeProductFormat(freeItem);
+            }
+        }
+        return totalPromotionDiscount;
+    }
+
+    private void printPriceDetails(NumberFormat currencyFormat) {
+        OutputView.printTotalAmount(totalPurchasePrice,currencyFormat);
+        OutputView.printPromotionDiscount(totalPromotionDiscount,currencyFormat);
+        OutputView.printMembershipDiscount(membershipDiscountPrice,currencyFormat);
+        OutputView.printFinalAmount(totalAmount,currencyFormat);
+    }
+}
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,10 +1,55 @@
 package store.view;
 
+import store.message.ReceiptMessage;
 import store.message.ViewMessage;
+import store.model.PurchaseItem;
+import store.model.Receipt;
+
+import java.text.NumberFormat;
 
 public class OutputView {
     public static void welcomeStore() {
-        System.out.println(ViewMessage.WELCOME_STORE);
-        System.out.println(ViewMessage.PRODUCT_LIST);
+        System.out.println(ViewMessage.WELCOME_STORE.getMessage());
+        System.out.println(ViewMessage.PRODUCT_LIST.getMessage());
+    }
+
+    public static void printStore() {
+        System.out.println(ReceiptMessage.WELCOME_STORE.getMessage());
+        System.out.println(ReceiptMessage.PRODUCT_LIST.getMessage());
+    }
+
+    public static void printFreeItem() {
+        System.out.println(ReceiptMessage.PROMOTION_HEADER.getMessage());
+    }
+
+    public static void printLine() {
+        System.out.println(ReceiptMessage.LINE.getMessage());
+    }
+
+    public static void printProductFormat(PurchaseItem purchaseItem, NumberFormat currencyFormat) {
+        System.out.printf(ReceiptMessage.PRODUCT_FORMAT.format(purchaseItem.getName(),
+                purchaseItem.getQuantity(),
+                currencyFormat.format(purchaseItem.getPrice())));
+    }
+
+    public static void printFreeProductFormat(PurchaseItem freeItem) {
+        System.out.printf(ReceiptMessage.FREE_ITEM_FORMAT.format(
+                freeItem.getName(), freeItem.getQuantity()));
+    }
+
+    public static void printTotalAmount(int totalPurchasePrice, NumberFormat currencyFormat) {
+        System.out.printf(ReceiptMessage.TOTAL_AMOUNT.format(currencyFormat.format(totalPurchasePrice)));
+    }
+
+    public static void printPromotionDiscount(int totalPromotionDiscount, NumberFormat currencyFormat) {
+        System.out.printf(ReceiptMessage.PROMOTION_DISCOUNT.format(currencyFormat.format(totalPromotionDiscount)));
+    }
+
+    public static void printMembershipDiscount(int membershipDiscountPrice, NumberFormat currencyFormat) {
+        System.out.printf(ReceiptMessage.MEMBERSHIP_DISCOUNT.format(currencyFormat.format(membershipDiscountPrice)));
+    }
+
+    public static void printFinalAmount(int totalAmount, NumberFormat currencyFormat) {
+        System.out.printf(ReceiptMessage.FINAL_AMOUNT.format(currencyFormat.format(totalAmount)));
     }
 }
